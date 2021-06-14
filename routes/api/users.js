a13b9cea436858590d117991ae7f0b989c31c3e3
Brenda J <linjiangda@yahoo.com>
2021-06-14 14:56:31 -0400

    user auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,22 +4,25 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+const validateSignupInput = require('../../validation/signup');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/users", (req, res) => res.json({
     msg: "This is the users index route!"
 }));
 
 router.post("/signup", (req, res) => {
-    // const { errors, isValid } = validateSignupInput(req.body);
+    const { errors, isValid } = validateSignupInput(req.body);
 
-    // if (!isValid) {
-    //     return res.status(400).json(errors);
-    // }
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
 
     User.findOne({ username: req.body.username }).then(user => {
         if (user) {
-            errors.username = "User already exists";
-            return res.status(400).json(errors);
+            return res.status(400).json({ username: 'This user not found.' });
+
         } else {
             const newUser = new User({
                 username: req.body.username,
@@ -51,19 +54,18 @@ router.post("/signup", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-    // const { errors, isValid } = validateLoginInput(req.body);
+    const { errors, isValid } = validateLoginInput(req.body);
 
-    // if (!isValid) {
-    //     return res.status(400).json(errors);
-    // }
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
 
     const username = req.body.username;
     const password = req.body.password;
 
     User.findOne({ username }).then(user => {
         if (!user) {
-            errors.username = "This user does not exist";
-            return res.status(400).json(errors);
+            return res.status(404).json({ username: 'This user does not exist.' });
         }
 
         bcrypt.compare(password, user.password).then(isMatch => {
@@ -84,12 +86,21 @@ router.post("/login", (req, res) => {
     });
 });
 
-router.post("/demologin", (req, res) => {
-
-    const username = req.body.username;
-    const password = req.body.password;
-
-
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    res.json({
+        id: req.user.id,
+        username: req.user.username,
+        email: req.user.email
+    });
 })
+
+
+
+// router.post("/demologin", (req, res) => {
+
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+// })
 
 module.exports = router;
