592ee6ebf108cc5990a97e2c7a0fd2b8a009a809
msaia97 <masonsaia@gmail.com>
2021-06-14 10:36:50 -0700

    Completed setup

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+const mongoose = require("mongoose")
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Welcome to YouChoose"));
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const users = require("./routes/api/users");
+const restaurants = require("./routes/api/restaurants");
+const bodyParser = require("body-parser");
 const db = require("./config/keys").mongoURI;
+const port = process.env.PORT || 5000;
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Welcome to YouChoose"));
+app.use("/api/users", users);
+app.use("/api/restaurants", restaurants);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
