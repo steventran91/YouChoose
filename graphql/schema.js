ecbddaa674685f1f12c632c778c1e62bd9d9d7c0
StevenTran91 <sdtran91@gmail.com>
2021-06-15 10:27:25 -0700

    add yelp api

---
+++ graphql/schema.js
@@ -0,0 +1,16 @@
+// const { GraphQLObjectType } = require('graphql');
+
+// const Restaurant = new GraphQLObjectType({
+//     name: 'Restaurant',
+//     fields: () => ({
+//         business[x].name: { type: String },
+//         open_now: { type: Boolean },
+//         business[x].price: { type: String },
+//         business[x].rating: { type: Decimal },
+//         business[x].location: { type: Object },
+
+//     })
+// })
+
+
+
