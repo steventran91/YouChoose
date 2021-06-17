3bafa69e1428b1fd05e47aeca9ae60f7fdb7b4aa
Brenda J <linjiangda@yahoo.com>
2021-06-17 19:48:22 -0400

    Stars

--- frontend/src/components/restaurants/restaurant_list.js
+++ frontend/src/components/restaurants/restaurant_list.js
@@ -8,41 +8,56 @@ class RestaurantList extends React.Component {
             index: 0
         }
         this.handleIndex = this.handleIndex.bind(this);
+        // this.setRating = this.setRating.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         // debugger
         let cuisine = this.props.cuisine
         let region = this.props.region
         console.log(cuisine)
         console.log(region)
-        this.props.fetchRestaurantsByCuisine({cuisine: cuisine, location: region});
+        this.props.fetchRestaurantsByCuisine({ cuisine: cuisine, location: region });
     }
 
     componentDidUpdate() {
         console.log(this.props.cuisine)
     }
 
-    handleIndex(index){
+    handleIndex(index) {
         return e => {
             e.preventDefault();
-            this.setState({index})
+            this.setState({ index })
         }
     }
 
-    render(){
+    // setRating(rating) {
+    //     { document.getElementsByClassName(`.stars-inner`).style.width = rating }
+    // }
+
+    render() {
+        // document.getElementsByClassName(`.stars-inner`).style.width = this.props.restaurant.rating
+
         if (this.props.restaurants) {
-            return(
+            return (
                 <div className="restaurant-page">
                     <ul className="restaurant-list">
                         {Object.values(this.props.restaurants).map((restaurant, index) => {
                             // return <RestaurantContainer restaurant={restaurant} />
-                            return <li 
-                                    className={this.state.index === index ? "active" : ""}
-                                    onClick={this.handleIndex(index)}
-                                    key={index}>
-                                        {restaurant.name}
-                                    </li>
+                            return <li
+                                className={this.state.index === index ? "active" : ""}
+                                onClick={this.handleIndex(index)}
+                                key={index}>
+                                {restaurant.name}
+                                <br />
+                                <div className="star-ratings">
+                                    <div className="star-ratings-css-product">
+                                        <div className="star-ratings-css-product-top" style={{ width: `${parseFloat(restaurant.rating).toFixed(1) * 0.83}em` }}><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+                                        <div className="star-ratings-css-product-bottom"><span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+                                    </div>
+                                </div>
+                                {restaurant.rating}
+                            </li>
                         })}
                     </ul>
                     <div className="restaurant-show">
@@ -56,7 +71,6 @@ class RestaurantList extends React.Component {
                     <p>hi</p>
                 </div>
             )
-
         }
     }
 }
