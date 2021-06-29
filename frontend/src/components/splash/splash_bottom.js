ae458a0728f9b8b4bd078cd4e2cf7939600dfdba
Brenda J <linjiangda@yahoo.com>
2021-06-29 14:47:59 -0400

    pictures

--- frontend/src/components/splash/splash_bottom.js
+++ frontend/src/components/splash/splash_bottom.js
@@ -1,4 +1,8 @@
 import React from "react";
+import steven from "./images/steven.jpg"
+import juan from "./images/juan.jpg"
+import mason from "./images/mason.jpg"
+import brenda from "./images/brenda.jpg"
 
 class SplashBottom extends React.Component {
     // constructor(props) {
@@ -12,25 +16,36 @@ class SplashBottom extends React.Component {
                     <h1 id="splash-bottom">Meet Our Developers</h1>
                     <div className="developers">
                         <div className="steven">
+                            <img className="our-pics" src={steven} />
                             Steven Tran
                             <br />
-                            <a href="https://github.com/steventran91">Github</a>
+                            <a href="https://github.com/steventran91"><i class="fa">&#xf09b;</i></a>
                         </div>
                         <br />
                         <div className="juan">
+                        <img className="our-pics" src={juan} />
                             Juan Barba
                             <br />
-                            <a href="https://github.com/JuanMBarba">Github</a>
+                            <a href="https://github.com/JuanMBarba"><i class="fa">&#xf09b;</i></a>
                         </div>
                         <div className="mason">
+                        <img className="our-pics" src={mason} />
                             Mason Saia
                             <br />
-                            <a href="https://github.com/msaia97">Github</a>
+                            <a href="https://github.com/msaia97"><i class="fa">&#xf09b;</i></a>
                         </div>
                         <div className="brenda">
+                        <img className="our-pics" src={brenda} />
+                        {/* <div className="brenda-1"> */}
+                            {/* <span className="brenda-main">Brenda Jiang
+                                <span className="brenda-hover">
+                                    Is there any text here?
+                            </span>
+                            </span> */}
                             Brenda Jiang
+                            {/* </div> */}
                             <br />
-                            <a href="https://github.com/Brenda-J">Github</a>
+                            <a href="https://github.com/Brenda-J"><i class="fa">&#xf09b;</i></a>
                         </div>
                     </div>
                 </div>
