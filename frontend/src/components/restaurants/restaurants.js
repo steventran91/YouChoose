db091d49b04fae70af5759cbf0691561c5ce4493
Juan Barba <barbajuan11@gmail.com>
2021-07-08 13:58:59 -0700

    Update show page

--- frontend/src/components/restaurants/restaurants.js
+++ frontend/src/components/restaurants/restaurants.js
@@ -34,11 +34,14 @@ class Restaurant extends React.Component {
               <div className="fav-but-container">
                 <button className={this.props.currentUser.favorites[this.props.restaurant.id] ? "fav-button-act" : "fav-but"} onClick={(e => this.addToFavorite(e))}></button>
               </div>
-              <a href={this.props.restaurant.url} target="_blank">Go to Yelp: {this.props.restaurant.name}</a>
-              <p>Phone: {this.props.restaurant.phone}</p>
-              <p>Address: {this.props.restaurant.location.display_address}</p>
-              <p>Price: {this.props.restaurant.price}</p>
-              <p>Rating: {this.props.restaurant.rating}</p>
+              <div className="restaurant-info-right">
+                <h1 className="restaurant-title">{this.props.restaurant.name}</h1>
+                <a className="yelp-page-link" href={this.props.restaurant.url} target="_blank">Go to {this.props.restaurant.name} Yelp Page!</a>
+                <p>Phone: {this.props.restaurant.phone}</p>
+                <p>Address: {this.props.restaurant.location.display_address}</p>
+                <p>Price: {this.props.restaurant.price}</p>
+                <p>Rating: {this.props.restaurant.rating}</p>
+              </div>
             </div>
           </div>
         </div>
